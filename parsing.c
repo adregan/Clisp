c4f611bcc6a1d8eba0d7d700eff16e2c82cd0bf7
Duncan Regan <duncanregan@gmail.com>
2016-05-21 20:19:00 -0400

    bumps version number

--- parsing.c
+++ parsing.c
@@ -44,7 +44,7 @@ int main(int argc, char** argv) {
     Number, Operator, Expr, Clisp);
 
   // Version information, &c.
-  puts("Clisp v. 0.0.0.0.0.1.0.0");
+  puts("Clisp v. 0.0.0.0.0.1.1.0");
   puts("☕  & 🍔 ");
   puts("Press ⌃ C to Exit");
 
